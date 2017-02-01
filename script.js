41e8b31c5def0b1eeef5adab46952b89e28f9400
Jennifer <jenniferzheng23@gmail.com>
2017-02-01 21:43:05 +0000

    ake a rectangle

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ TinyTurtle.apply(window);
 right(90);
 forward(60);
 left(90);
-forward(40);
+forward(60);
 left(90);
-forward(40);
-right(180);
-forward(80);
+forward(60);
+left(90);
+forward(60);
  
-
-
-
-stamp();
-stamp(); 
